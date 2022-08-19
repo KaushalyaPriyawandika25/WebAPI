bf9c786988660edd39dee6df6bc884f3f51ea723
USER <USER@DESKTOP-2GO45DL>
2022-08-19 23:47:11 +0530

    Add project files.

---
+++ WebAPI/Data/WebAPIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class WebAPIDbContext : DbContext
+    {
+        public WebAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+       public DbSet<Contact> Contacts { get; set; }
+
+    }
+}
